52096e31c4a8d316d5489810a97fa3b89bf6aea3
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2025-06-30 11:52:14 -0300

    Update cards.js
    add car kiel low

--- data/cards.js
+++ data/cards.js
@@ -86,6 +86,15 @@ export const cards = [
         }
     },
     {
+        id: '4403',
+        dbname: 'Kiel_Card',
+        name: 'Carta Kiel-D-01',
+        position: 'low',
+        script: function () {
+            equipStats.castdelay += 30;
+        }
+    },    
+    {
         id: '4556',
         dbname: 'Fenrir_Card',
         name: 'Carta Fenrir',
